8bcc583052a728d90d038e8bffb534fa891fef00
hyunseo01 <hs001023@naver.com>
2025-08-22 12:44:19 +0900

    new commit

--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,25 @@
+import 'reflect-metadata';
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { ValidationPipe } from '@nestjs/common';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  app.use((req, res, next) => {
+    const t = Date.now();
+    res.on('finish', () => {
+      console.log(
+        `[REQ] ${req.method} ${req.url} -> ${res.statusCode} (${Date.now() - t}ms)`,
+      );
+    });
+    next();
+  });
+
+  const express = app.getHttpAdapter().getInstance();
+  express.get('/', (_req, res) => {
+    res.json({ ok: true, msg: 'hello world (raw)' });
+  });
 
   app.enableCors();
   app.useGlobalPipes(new ValidationPipe({ whitelist: true, transform: true }));
