1eb63da4e944bc700cd9f90251cf4be0592f7076
hyunseo01 <hs001023@naver.com>
2025-08-23 12:18:32 +0900

    new commit

--- src/main.ts
+++ src/main.ts
@@ -6,6 +6,17 @@ import { ValidationPipe } from '@nestjs/common';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
+  app.enableCors({
+    origin: [
+      'http://localhost:3000',
+      'http://127.0.0.1:3000',
+      'https://<프론트_배포_도메인>',
+    ],
+    methods: ['GET', 'POST', 'PATCH', 'PUT', 'DELETE', 'OPTIONS'],
+    allowedHeaders: ['Content-Type', 'Authorization'],
+    credentials: true,
+  });
+
   app.use((req, res, next) => {
     const t = Date.now();
     res.on('finish', () => {
